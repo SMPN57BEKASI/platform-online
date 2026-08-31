b65a89ae511fe73eaf662691d55b5ad010444df7
SMP Negeri 57 Bekasi <smpnegeri57bekasi@gmail.com>
2026-08-31 14:31:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,77 +16,77 @@ const driveLinks = [
         title: "Modul Ajar Matematika",
         description: "Kumpulan modul ajar Matematika kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "📐",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar Bahasa Indonesia",
         description: "Kumpulan modul ajar Bahasa Indonesia kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "🖋️",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar Bahasa Sunda",
         description: "Kumpulan modul ajar Bahasa Sunda kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "🎭",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar Bahasa Inggris",
         description: "Kumpulan modul ajar Bahasa Inggris kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "🔡",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar IPA",
         description: "Kumpulan modul ajar IPA kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "🧬",
-        url: "https://drive.google.com/"
+        url: ""
     }, 
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar IPS",
         description: "Kumpulan modul ajar IPS kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "🗺️",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar Informatika",
         description: "Modul ajar Informatika untuk kelas menengah pertama.",
         icon: "💻",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar PABP",
         description: "Kumpulan modul ajar PABP kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "👳🏻‍♂️",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar Pendidikan Pancasila",
         description: "Kumpulan modul ajar Pendidikan Pancasila kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "⚖️",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar Seni Budaya",
         description: "Kumpulan modul ajar Seni Budaya kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "🎨",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Perangkat Pembelajaran",
         title: "Modul Ajar PJOK",
         description: "Kumpulan modul ajar PJOK kelas 7, 8, dan 9 Kurikulum Merdeka.",
         icon: "⚽",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Administrasi",
@@ -100,41 +100,39 @@ const driveLinks = [
         title: "Program Kerja Sekolah",
         description: "Dokumen rancangan kegiatan dan program kerja tahunan.",
         icon: "🏢",
-        url: "https://drive.google.com/"
+        url: ""
     },
       {
         category: "Dokumen Sekolah",
         title: "Program Kerja OSIS",
         description: "Dokumen rancangan kegiatan dan program kerja tahunan.",
         icon: "🏢",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Penilaian",
         title: "Kisi-kisi & Soal Ujian",
         description: "Kumpulan soal Asesmen Sumatif Tengah dan Akhir Semester.",
         icon: "📝",
-        url: "https://drive.google.com/"
+        url: ""
     },
       {
         category: "Dokumen Sekolah",
         title: "Karya Tulis Guru",
         description: "Kumpulan karya tulis Guru SMPN 57 Bekasi",
         icon: "📝",
-        url: "https://drive.google.com/"
+        url: ""
     },
     {
         category: "Dokumen Sekolah",
         title: "Kalender Akademik",
         description: "Jadwal kegiatan belajar mengajar dan hari libur sekolah.",
         icon: "🗓️",
-        url: "https://drive.google.com/"
+        url: ""
     }
 ];
 
-/* =========================================
-   LOGIKA SISTEM (Jangan diubah kecuali paham JS)
-   ========================================= */
+/*========================================= */
 
 // DOM Elements
 const driveGrid = document.getElementById('driveGrid');
